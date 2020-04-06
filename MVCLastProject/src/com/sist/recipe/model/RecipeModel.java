90e03c10e20182ebad4e4783876897c4551e72cc
daegeba <shinsh29@gmail.com>
2020-04-06 18:53:52 +0900

    MVC구조를 이용한 메인페이지(04/06)

---
+++ MVCLastProject/src/com/sist/recipe/model/RecipeModel.java
@@ -0,0 +1,102 @@
+package com.sist.recipe.model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sist.controller.Controller;
+import com.sist.controller.RequestMapping;
+import com.sist.dao.RecipeDAO;
+
+import java.util.*;
+import com.sist.vo.*;
+
+@Controller
+public class RecipeModel {
+
+	@RequestMapping("recipe/recipe.do")
+	public String recipe_recipe(HttpServletRequest request, HttpServletResponse response) 
+	{
+		//요청=> 처리 => DAO
+		String page=request.getParameter("page");
+		if(page==null)
+			page="1";
+		int curpage=Integer.parseInt(page);
+		int rowSize=20;
+		int start=(rowSize*curpage)-(rowSize-1);
+		int end=(rowSize*curpage);
+		
+		Map map=new HashMap();
+		map.put("start", start);
+		map.put("end", end);
+		
+		List<RecipeVO> list=RecipeDAO.recipeListData(map);
+		for(RecipeVO vo:list)
+		{
+			String title=vo.getTitle();
+			if(title.length()>13)
+			{
+				title=title.substring(0,13).concat("...");
+				vo.setTitle(title);
+			}
+		}
+		int totalpage=RecipeDAO.recipeTotalPage();
+		
+		final int BLOCK=10;
+		int startPage=((curpage-1)/BLOCK*BLOCK)+1;
+		int endPage=((curpage-1)/BLOCK*BLOCK)+BLOCK;
+		//1~10, 11~20
+		int allPage=totalpage;
+		if(endPage>allPage) endPage=allPage;
+		
+		request.setAttribute("list", list);
+		request.setAttribute("curpage", curpage);
+		request.setAttribute("totalpage", totalpage);
+		request.setAttribute("BLOCK", BLOCK);
+		request.setAttribute("startPage", startPage);
+		request.setAttribute("endPage", endPage);
+		request.setAttribute("allPage", allPage);
+		//request처리하는 부분
+		request.setAttribute("main_jsp", "../recipe/recipe.jsp");
+		//includ가 있는 부분
+		return "../main/main.jsp";
+	}
+	
+	//요청이 온 곳
+	@RequestMapping("recipe/chef.do")
+	public String recipe_chef(HttpServletRequest request, HttpServletResponse response)
+	{
+		//페이지 구분
+		String page=request.getParameter("page");
+		if(page==null)
+			page="1";
+		int curpage=Integer.parseInt(page);
+		int rowSize=30;
+		int start=(rowSize*curpage)-(rowSize-1);
+		int end=(rowSize*curpage);
+		
+		Map map=new HashMap();
+		map.put("start", start);
+		map.put("end", end);
+		
+		List<ChefVO> list=RecipeDAO.chefListData(map);
+		int totalpage=RecipeDAO.chefTotalPage();
+		
+		//한 줄 출력페이지
+		final int BLOCK=5;
+		int startPage=((curpage-1)/BLOCK*BLOCK)+1;
+		int endPage=((curpage-1)/BLOCK*BLOCK)+BLOCK;
+		//1~10, 11~20
+		int allPage=totalpage;
+		if(endPage>allPage) endPage=allPage;
+		
+		request.setAttribute("list", list);
+		request.setAttribute("curpage", curpage);
+		request.setAttribute("totalpage", totalpage);
+		request.setAttribute("BLOCK", BLOCK);
+		request.setAttribute("startPage", startPage);
+		request.setAttribute("endPage", endPage);
+		request.setAttribute("allPage", allPage);
+		request.setAttribute("main_jsp", "../recipe/chef.jsp");
+		return "../main/main.jsp";
+	}
+}
